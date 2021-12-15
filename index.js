5b4b12266c4c48d46088cddc121c5f532bd08f6b
Samyog Dhital <dhitalsamyog123@gmail.com>
2021-12-15 20:11:51 +0545

    check

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://www.samratdhital.com.np/api/",
+    origin: "http://localhost:3000/",
     methods: ["GET", "POST"],
   },
 });
